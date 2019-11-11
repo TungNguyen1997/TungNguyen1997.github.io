6d1f599e073bdbd346c41cb838cd4fc249f979df
TungNguyen1997 <33158187+TungNguyen1997@users.noreply.github.com>
2019-11-11 15:02:15 +0700

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded",function() {
+	var nut = document.getElementsByClassName('declick');
+	var nd=document.getElementsByClassName('noidung');
+	console.log(nut);
+	for (var i = 0; i < nut.length; i++) {
+		
+		nut[i].addEventListener('click',function(){
+			var y = this.getAttribute('data-hienlen');
+			var z = document.getElementById(y);
+			if(this.classList[1] == 'trang')
+			{
+				this.classList.remove('trang');
+				z.classList.remove('hienra');
+
+			}
+			else{
+				for (var k = 0; k < nut.length; k++) {
+				nut[k].classList.remove('trang');
+			}
+			this.classList.toggle('trang');
+			
+			
+			console.log(z);
+			for (var u = 0; u< nd.length; u++) {
+
+				nd[u].classList.remove('hienra');
+			
+				z.classList.add('hienra');
+						}
+			
+		}
+		})
+			
+			}\
+			
+	}
+
+},false)
+	// body...
